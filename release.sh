af4a6a012948523de2033512df5ecee32e3ef47c
Andrew Cain <macite@gmail.com>
2021-08-03 17:10:58 +1000

    fix: correct issue with release script use of select

--- release.sh
+++ release.sh
@@ -30,6 +30,8 @@ echo
 echo "### Step 2. Update version tags"
 echo 
 
+echo "Doubtfire is at version `git describe --abbrev=0 --tags`"
+
 select answer in "Full" "Prerelease"; do
   case $answer in
     Full)
@@ -42,47 +44,45 @@ select answer in "Full" "Prerelease"; do
   esac
 done
 
+SKIP=0
+
 select answer in "Skip" "Auto" "Major" "Minor" "Patch"; do
+  case $answer in
+    Skip)
+      SKIP=1
+      break;
+      ;;
+    Auto)
+      RELEASE_AS=
+      break;
+      ;;
+    Major)
+      RELEASE_AS="--release-as major"
+      break;
+      ;;
+    Minor)
+      RELEASE_AS="--release-as minor"
+      break;
+      ;;
+    Patch)
+      RELEASE_AS="--release-as patch"
+      break;
+      ;;
+  esac
+done
 
   function prepare_release {
     PROJECT=$1
     PROJECT_PATH=$2
 
     cd "${PROJECT_PATH}"
-
-    echo "${PROJECT} is at version `git describe --abbrev=0 --tags`"
-    case $answer in
-      Skip)
-        echo "Not changing ${PROJECT} tag"
-        break;
-        ;;
-      Auto)
-        echo "Creating ${PROJECT} release and bumping the major version"
-        standard-version $PRERELEASE
-        break;
-        ;;
-      Major)
-        echo "Creating ${PROJECT} release and bumping the major version"
-        standard-version --release-as major $PRERELEASE
-        break;
-        ;;
-      Minor)
-        echo "Creating ${PROJECT} release and bumping the minor version"
-        standard-version --release-as minor $PRERELEASE
-        break;
-        ;;
-      Patch)
-        echo "Creating ${PROJECT} release and bumping the patch version"
-        standard-version --release-as patch $PRERELEASE
-        break;
-        ;;
-    esac
+    standard-version $RELEASE_AS $PRERELEASE
 
     CURRENT_BRANCH=$(git branch --show-current)
     RELEASE_VERSION=`git describe --abbrev=0 --tags`
     while [ $CURRENT_BRANCH != $RELEASE_VERSION ]; do
       echo "$PROJECT does not match release branch naming: $CURRENT_BRANCH != $RELEASE_VERSION"
-      read -p "Fix then press enter to continue"
+      read -p "Fix then press enter to continue (or break to quit)"
 
       CURRENT_BRANCH=$(git branch --show-current)
       RELEASE_VERSION=`git describe --abbrev=0 --tags`
@@ -125,13 +125,16 @@ echo
 echo "### Step 4: Push releases"
 echo
 
+echo "What's the name of the remote to push to (doubtfire-lms)"
+read -p "Remote: (eg origin/upstream): " REMOTE
+
 function push_release {
   PROJECT=$1
   PROJECT_PATH=$2
 
   cd "${PROJECT_PATH}"
   CURRENT_BRANCH=$(git branch --show-current)
-  git push --follow-tags origin ${CURRENT_BRANCH}:releases
+  git push --follow-tags $REMOTE ${CURRENT_BRANCH}:releases
   if [ $? -ne 0 ]; then
     echo "Oh no... fix up this mess please";
     exit 1
@@ -140,18 +143,18 @@ function push_release {
 
 echo "Push new releases to GitHub?"
 select answer in "Skip" "Push"; do
-    case $answer in
-        Skip)
-          echo "Not pushing releases"
-          break;
-          ;;
-        Push)
-          push_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
-          push_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
-          push_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
-          push_release 'doubtfire-deploy' "${APP_PATH}"
-          break;
-          ;;
-    esac
+  case $answer in
+    Skip)
+      echo "Not pushing releases"
+      break;
+      ;;
+    Push)
+      push_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
+      push_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
+      push_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
+      push_release 'doubtfire-deploy' "${APP_PATH}"
+      break;
+      ;;
+  esac
 done
 
