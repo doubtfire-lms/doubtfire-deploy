7605eea674bfbfe6de387018ad242d7569767556
Andrew Cain <macite@gmail.com>
2021-08-05 11:24:03 +1000

    chore: merge 5.0.x release candidate changes
    * fix: correct issue with release script use of select
    
    * feat: add script to change remotes of submodules
    
    * fix: correct comparison of the release and current branch names
    
    * fix: correct use of while in branch test
    
    * chore: correct doubtfire-web submodule reference
    
    * chore: update web version with lint fixes
    
    * chore(release): null
    
    * fix: correct syntax in release script and order of deploy changes
    
    * chore: update web package file version
    
    * fix: correct overseer change log path for release
    
    * chore: prepare version 5.0.0-0 release
    
    * chore(release): null
    
    * chore: fix version in package file
    
    * fix: move release notes to match release number
    
    * fix: correct issue with release checking branch vs release number
    
    * fix: correct readme details for release notes
    
    * feat: switch build process to use docker for building the web server
    
    * chore: switch event from push to deployment
    
    * fix: correct event syntax in workflow
    
    * fix: change name for deployment action
    
    * config: allow deployment workflow to be dispatched manually

--- release.sh
+++ release.sh
@@ -30,6 +30,8 @@ echo
 echo "### Step 2. Update version tags"
 echo 
 
+echo "Doubtfire is at version `git describe --abbrev=0 --tags`"
+
 select answer in "Full" "Prerelease"; do
   case $answer in
     Full)
@@ -42,84 +44,85 @@ select answer in "Full" "Prerelease"; do
   esac
 done
 
+SKIP=0
+
 select answer in "Skip" "Auto" "Major" "Minor" "Patch"; do
+  case $answer in
+    Skip)
+      SKIP=1
+      break;
+      ;;
+    Auto)
+      RELEASE_AS=
+      break;
+      ;;
+    Major)
+      RELEASE_AS="--release-as major"
+      break;
+      ;;
+    Minor)
+      RELEASE_AS="--release-as minor"
+      break;
+      ;;
+    Patch)
+      RELEASE_AS="--release-as patch"
+      break;
+      ;;
+  esac
+done
 
-  function prepare_release {
-    PROJECT=$1
-    PROJECT_PATH=$2
+function prepare_release {
+  PROJECT=$1
+  PROJECT_PATH=$2
 
-    cd "${PROJECT_PATH}"
+  cd "${PROJECT_PATH}"
+  standard-version $RELEASE_AS $PRERELEASE
 
-    echo "${PROJECT} is at version `git describe --abbrev=0 --tags`"
-    case $answer in
-      Skip)
-        echo "Not changing ${PROJECT} tag"
-        break;
-        ;;
-      Auto)
-        echo "Creating ${PROJECT} release and bumping the major version"
-        standard-version $PRERELEASE
-        break;
-        ;;
-      Major)
-        echo "Creating ${PROJECT} release and bumping the major version"
-        standard-version --release-as major $PRERELEASE
-        break;
-        ;;
-      Minor)
-        echo "Creating ${PROJECT} release and bumping the minor version"
-        standard-version --release-as minor $PRERELEASE
-        break;
-        ;;
-      Patch)
-        echo "Creating ${PROJECT} release and bumping the patch version"
-        standard-version --release-as patch $PRERELEASE
-        break;
-        ;;
-    esac
+  CURRENT_BRANCH=$(git branch --show-current)
+  RELEASE_VERSION=`git describe --abbrev=0 --tags`
+  TRUNC_RELEASE=${RELEASE_VERSION#v}
+
+  while [ ${CURRENT_BRANCH%.x} != ${TRUNC_RELEASE%.*} ]; do
+    echo "$PROJECT does not match release branch naming: $CURRENT_BRANCH != $RELEASE_VERSION"
+    read -p "Fix then press enter to continue (or break to quit)"
 
     CURRENT_BRANCH=$(git branch --show-current)
     RELEASE_VERSION=`git describe --abbrev=0 --tags`
-    while [ $CURRENT_BRANCH != $RELEASE_VERSION ]; do
-      echo "$PROJECT does not match release branch naming: $CURRENT_BRANCH != $RELEASE_VERSION"
-      read -p "Fix then press enter to continue"
-
-      CURRENT_BRANCH=$(git branch --show-current)
-      RELEASE_VERSION=`git describe --abbrev=0 --tags`
-    done
-    echo
-  }
-
-  prepare_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
-  WEB_VERSION=$(git describe --abbrev=0 --tags)
-
-  prepare_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
-  API_VERSION=$(git describe --abbrev=0 --tags)
-
-  prepare_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
-  OVERSEER_VERSION=$(git describe --abbrev=0 --tags)
-
+    TRUNC_RELEASE=${RELEASE_VERSION#v}
+  done
   echo
-  echo "### Step 3: Prepare deploy for release"
-  echo
+}
 
-  cd "${APP_PATH}/releases"
-  DATE_WITH_TIME=`date "+%Y-%m%d-%H%M"`
-  mkdir $DATE_WITH_TIME
-  echo "$API_VERSION" > "${DATE_WITH_TIME}/.apiversion"
-  echo "$WEB_VERSION" > "${DATE_WITH_TIME}/.webversion"
-  echo "$OVERSEER_VERSION" > "${DATE_WITH_TIME}/.overseer"
-  cp -r ./release-template/. ./${DATE_WITH_TIME}
-  echo "https://github.com/doubtfire-lms/doubtfire-web/blob/${WEB_VERSION}/CHANGELOG.md" > ${DATE_WITH_TIME}/WEB_CHANGELOG.md
-  echo "https://github.com/doubtfire-lms/doubtfire-api/blob/${API_VERSION}/CHANGELOG.md" > ${DATE_WITH_TIME}/API_CHANGELOG.md
-  echo "https://github.com/doubtfire-lms/doubtfire-api/blob/${OVERSEER_VERSION}/CHANGELOG.md" > ${DATE_WITH_TIME}/OVERSEER_CHANGELOG.md
+prepare_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
+WEB_VERSION=$(git describe --abbrev=0 --tags)
 
-  echo
-  echo "Please update release notes, and push them to origin before continuing here..."
+prepare_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
+API_VERSION=$(git describe --abbrev=0 --tags)
 
-  prepare_release 'doubtfire-deploy' "${APP_PATH}"
-  DEPLOY_VERSION=$(git describe --abbrev=0 --tags)
-done
+prepare_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
+OVERSEER_VERSION=$(git describe --abbrev=0 --tags)
+
+echo
+echo "### Step 3: Prepare deploy for release"
+echo
+
+cd "${APP_PATH}/releases"
+mkdir -p $RELEASE_VERSION
+echo "$API_VERSION" > "${RELEASE_VERSION}/.apiversion"
+echo "$WEB_VERSION" > "${RELEASE_VERSION}/.webversion"
+echo "$OVERSEER_VERSION" > "${RELEASE_VERSION}/.overseer"
+cp -r ./release-template/. ./${RELEASE_VERSION}
+echo "https://github.com/doubtfire-lms/doubtfire-web/blob/${WEB_VERSION}/CHANGELOG.md" > ${RELEASE_VERSION}/WEB_CHANGELOG.md
+echo "https://github.com/doubtfire-lms/doubtfire-api/blob/${API_VERSION}/CHANGELOG.md" > ${RELEASE_VERSION}/API_CHANGELOG.md
+echo "https://github.com/doubtfire-lms/doubtfire-overseer/blob/${OVERSEER_VERSION}/CHANGELOG.md" > ${RELEASE_VERSION}/OVERSEER_CHANGELOG.md
+
+echo
+echo "Please update release notes, and push them to origin before continuing here..."
+
+read -p "Press enter to continue" TEMP
+
+prepare_release 'doubtfire-deploy' "${APP_PATH}"
+DEPLOY_VERSION=$(git describe --abbrev=0 --tags)
 
 echo
 echo "### Step 4: Push releases"
@@ -131,7 +134,7 @@ function push_release {
 
   cd "${PROJECT_PATH}"
   CURRENT_BRANCH=$(git branch --show-current)
-  git push --follow-tags origin ${CURRENT_BRANCH}:releases
+  git push --follow-tags $REMOTE ${CURRENT_BRANCH}:releases
   if [ $? -ne 0 ]; then
     echo "Oh no... fix up this mess please";
     exit 1
@@ -140,18 +143,21 @@ function push_release {
 
 echo "Push new releases to GitHub?"
 select answer in "Skip" "Push"; do
-    case $answer in
-        Skip)
-          echo "Not pushing releases"
-          break;
-          ;;
-        Push)
-          push_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
-          push_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
-          push_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
-          push_release 'doubtfire-deploy' "${APP_PATH}"
-          break;
-          ;;
-    esac
+  case $answer in
+    Skip)
+      echo "Not pushing releases"
+      break;
+      ;;
+    Push)
+      echo "What's the name of the remote to push to (doubtfire-lms)"
+      read -p "Remote: (eg origin/upstream): " REMOTE
+
+      push_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
+      push_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
+      push_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
+      push_release 'doubtfire-deploy' "${APP_PATH}"
+      break;
+      ;;
+  esac
 done
 
---
+++ releases/v5.0.0-0/OVERSEER_CHANGELOG.md
@@ -0,0 +1,1 @@
+https://github.com/doubtfire-lms/doubtfire-overseer/blob/v5.0.0-0/CHANGELOG.md
