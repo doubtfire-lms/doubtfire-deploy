b032b6f0092ea7fdcdeb96a99674a48016207522
Thomas Laing <thomaslaing50@gmail.com>
2021-09-18 07:23:59 +1000

    message

---
+++ src/app/tasks/task-status-selector/task-status-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'taskStatusSelector',
+  templateUrl: 'task-status-selector.component.html',
+  styleUrls: ['task-status-selector.component.scss']
+})
+export class TaskDescriptionCardComponent {
+
+  constructor(
+  ) {
+  }
+
+}
