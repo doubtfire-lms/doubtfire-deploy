eaf74e8a315762d026d0fc7f9c21268d593d9b93
Thomas Laing <thomaslaing50@gmail.com>
2021-09-23 00:05:52 +1000

    comment added

--- src/app/tasks/task-status-selector/task-status-selector.component.ts
+++ src/app/tasks/task-status-selector/task-status-selector.component.ts
@@ -1,14 +1,40 @@
 import { Component, Input, Inject } from '@angular/core';
+import { taskService } from '../../ajs-upgraded-providers';
+
 
 @Component({
   selector: 'taskStatusSelector',
   templateUrl: 'task-status-selector.component.html',
   styleUrls: ['task-status-selector.component.scss']
 })
-export class TaskDescriptionCardComponent {
 
-  constructor(
-  ) { //Hello there, I'm a comment. I don't change the function of the file but I'm different enought to care.
-  }
+export class TaskStatusSelectorComponent 
+{
+    task: "=task";
+    assessingUnitRole: "=assessingUnitRole";
+    inMenu: "=inMenu";
+    triggerTransition: "=triggerTransition";
 
+    private service: any;
+    private studentTriggers: any;
+    private tutorTriggers: any;
+
+
+    constructor(@Inject(taskService) service: any)
+    {
+        this.service = taskService;
+    }
+
+    studentStatuses(){
+        return this.service.switchableStates.student
+    }
+
+    tutorStatuses(){
+        return this.service.switchableStates.tutor
+    }
+
+    taskEngagementConfiguration(){
+        studentTriggers: this.studentStatuses.map; this.service.statusData
+        tutorTriggers: this.tutorStatuses.map; this.service.statusData
+    }
 }
