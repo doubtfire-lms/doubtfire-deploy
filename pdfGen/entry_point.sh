2f9ca9c99ef71a5a1231e810a8a13e9c9b47441a
Andrew Cain <macite@gmail.com>
2021-12-03 11:46:24 +1100

    feat: add scheduled cron tasks for repeated actions
    - Generates PDFs every few minutes
    - Checks plagiarism every week
    - Sends weekly emails
    - Synchronises enrolments every day

---
+++ pdfGen/entry_point.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start the run once job.
+echo "Docker container has been started"
+
+# Save the docker user environment
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+cat /container.env
+
+# Ensure log is present
+touch /var/log/cron.log
+
+sleep 1
+
+# Setup crontab - clear then load with file
+crontab -r
+crontab /etc/cron.d/container_cronjob
+
+echo "RESET CRONTAB" >> /var/log/cron.log
+
+
+# Run cron and follow log
+chmod 644 /etc/cron.d/container_cronjob && cron -f && tail -f /var/log/cron.log > /proc/1/fd/1 2>/proc/1/fd/2
