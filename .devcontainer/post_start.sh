243eb87b77409c2602bd400cdd39541c382d1edd
Andrew Cain <macite@gmail.com>
2023-02-01 21:07:39 +1100

    chore: ensure database started in devcontainer

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -5,3 +5,10 @@ git config --global --add safe.directory /workspace/doubtfire-api
 git config --global --add safe.directory /workspace/doubtfire-web
 git config --global --add safe.directory /workspace
 
+git config --global submodule.recurse false
+
+if [[ $(pgrep -n mysqld) -ne 0 && $(pgrep -n redis-server) -ne 0 ]]; then
+  echo 'Database and Redis are running'
+else
+  /workspace/.devcontainer/post_create.sh
+fi
