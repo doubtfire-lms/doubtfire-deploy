9dae756b0c9369ce6a387ef6c88f36be5275d33f
Andrew Cain <macite@gmail.com>
2023-03-03 22:00:41 +1100

    fix: ensure release is functioning

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=`cd "$APP_PATH"; pwd`
@@ -87,7 +87,7 @@ function prepare_release {
   TRUNC_RELEASE=${RELEASE_VERSION#v}
 
   while [ ${CURRENT_BRANCH%.x} != ${TRUNC_RELEASE%.*} ]; do
-    echo "$PROJECT does not match release branch naming: $CURRENT_BRANCH != $RELEASE_VERSION"
+    echo "$PROJECT does not match release branch naming: ${CURRENT_BRANCH%.x} != ${TRUNC_RELEASE%.*}"
     read -p "Fix then press enter to continue (or break to quit)"
 
     CURRENT_BRANCH=$(git branch --show-current)
@@ -121,7 +121,7 @@ function do_release {
 
 prepare_release 'doubtfire-web' "${APP_PATH}/doubtfire-web" WEB_VERSION
 prepare_release 'doubtfire-api' "${APP_PATH}/doubtfire-api" API_VERSION
-prepare_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer" OVERSEER_VERSION
+# prepare_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer" OVERSEER_VERSION
 prepare_release 'doubtfire-deploy' "${APP_PATH}" DEPLOY_VERSION
 
 echo "All projects are now ready. Proceed to create tagged commits?"
@@ -140,7 +140,7 @@ done
 
 do_release 'doubtfire-web' "${APP_PATH}/doubtfire-web"
 do_release 'doubtfire-api' "${APP_PATH}/doubtfire-api"
-do_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
+# do_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer"
 
 echo
 echo "### Step 3: Prepare deploy for release"
@@ -150,11 +150,11 @@ cd "${APP_PATH}/releases"
 mkdir -p $DEPLOY_VERSION
 echo "$API_VERSION" > "${DEPLOY_VERSION}/.apiversion"
 echo "$WEB_VERSION" > "${DEPLOY_VERSION}/.webversion"
-echo "$OVERSEER_VERSION" > "${DEPLOY_VERSION}/.overseer"
+# echo "$OVERSEER_VERSION" > "${DEPLOY_VERSION}/.overseer"
 cp -r ./release-template/. ./${DEPLOY_VERSION}
 echo "https://github.com/doubtfire-lms/doubtfire-web/blob/${WEB_VERSION}/CHANGELOG.md" > ${DEPLOY_VERSION}/WEB_CHANGELOG.md
 echo "https://github.com/doubtfire-lms/doubtfire-api/blob/${API_VERSION}/CHANGELOG.md" > ${DEPLOY_VERSION}/API_CHANGELOG.md
-echo "https://github.com/doubtfire-lms/doubtfire-overseer/blob/${OVERSEER_VERSION}/CHANGELOG.md" > ${DEPLOY_VERSION}/OVERSEER_CHANGELOG.md
+# echo "https://github.com/doubtfire-lms/doubtfire-overseer/blob/${OVERSEER_VERSION}/CHANGELOG.md" > ${DEPLOY_VERSION}/OVERSEER_CHANGELOG.md
 
 echo
 echo "Please update release notes and commit before continuing..."
@@ -194,7 +194,7 @@ select answer in "Skip" "Push"; do
 
       push_release 'doubtfire-web' "${APP_PATH}/doubtfire-web" $REMOTE
       push_release 'doubtfire-api' "${APP_PATH}/doubtfire-api" $REMOTE
-      push_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer" $REMOTE
+      # push_release 'doubtfire-overseer' "${APP_PATH}/doubtfire-overseer" $REMOTE
       push_release 'doubtfire-deploy' "${APP_PATH}" $REMOTE
 
       break;
