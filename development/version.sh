6cada840104e7ad6762dd7a949064cddc0bb5606
Andrew Cain <macite@gmail.com>
2023-03-27 20:13:25 +1100

    chore: update version for development image

--- development/version.sh
+++ development/version.sh
@@ -4,6 +4,6 @@
 # When the configuration of a container changes, the version should be incremented.
 # Breaking changes should increment the major version.
 # Non-breaking changes should increment the minor version.
-export CURRENT_API_VERSION=7.0
-export CURRENT_WEB_VERSION=7.0
-export CURRENT_DEV_VERSION=7.0
+export CURRENT_API_VERSION=8.0
+export CURRENT_WEB_VERSION=8.0
+export CURRENT_DEV_VERSION=8.0
