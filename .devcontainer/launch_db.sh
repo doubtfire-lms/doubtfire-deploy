7a1a1bd7c1f6f52c8d330d924f0a38bcad3ca8bb
Andrew Cain <macite@gmail.com>
2023-07-07 15:21:40 +1000

    fix: ensure database is setup with vscode user on create

--- .devcontainer/launch_db.sh
+++ .devcontainer/launch_db.sh
@@ -199,7 +199,7 @@ docker_init_database_dir() {
     installArgs+=(--skip-test-db)
   fi
   # "Other options are passed to mysqld." (so we pass all "mysqld" arguments directly here)
-  mysql_install_db "${installArgs[@]}" "${@:2}" --default-time-zone=SYSTEM --enforce-storage-engine=
+  mysql_install_db "${installArgs[@]}" "${@:2}" --default-time-zone=SYSTEM --user=vscode --enforce-storage-engine=
   mysql_note "Database files initialized"
 }
 
