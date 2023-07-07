8197c926dd1e8d5f4244c071f5bf545cff44b23a
Andrew Cain <macite@gmail.com>
2023-07-07 15:25:14 +1000

    fix: ensure volumes are writable by vscode

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,8 +5,16 @@ ln -sf /workspace/.devcontainer/.zshrc $HOME
 ln -s /workspace/.devcontainer/.p10k.zsh $HOME
 ln -s /workspace/.devcontainer/.irbrc $HOME
 
+
+sudo chmod a+rw /var/lib/mysql
+sudo chmod a+rw /workspace/tmp
+
 echo 'Granting access to student work in the background'
 sudo find /student-work/* -exec chmod a+rw {} \; &
+
+sudo chmod a+rw /workspace/node_modules
+sudo chmod a+rw /workspace/doubtfire-web/node_modules
+sudo chmod a+rw /home/vscode/.gems
 
 echo 'Starting Services'
 /workspace/.devcontainer/docker-entrypoint.sh echo "Done"
