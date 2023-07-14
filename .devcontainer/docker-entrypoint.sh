f38c0c682e78b7eacbacaec1259c91402c50a002
Andrew Cain <macite@gmail.com>
2023-07-14 10:43:29 +1000

    fix: permissions and simulate of signoff
    - ensure student work is always vscode
    - ensure simnulate happens... and doesn't end

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -7,7 +7,7 @@ sudo -E /workspace/.devcontainer/launch_db.sh mysqld
 
 # Start the redis server
 mkdir -p /workspace/tmp/sidekiq-redis
-redis-server --dir /workspace/tmp/sidekiq-redis >>/workspace/tmp/redis.log 2>>/workspace/tmp/redis.log &
+sudo redis-server --dir /workspace/tmp/sidekiq-redis >>/workspace/tmp/redis.log 2>>/workspace/tmp/redis.log &
 
 # Wait for mysql to start then setup the database if needed
 /workspace/.devcontainer/setup_formatif_db.sh
