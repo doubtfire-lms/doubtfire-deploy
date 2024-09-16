08f7765f1d1cc86180922ca8ddb511971732f0cc
b0ink <40929320+b0ink@users.noreply.github.com>
2024-09-16 15:24:22 +1000

    fix: remove previous logs to fix permission issues

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -7,6 +7,7 @@ echo "Starting Formatif development container..."
 
 # Start the redis server
 mkdir -p /workspace/tmp/sidekiq-redis
+sudo rm -rf /workspace/tmp/redis.log
 sudo redis-server --dir /workspace/tmp/sidekiq-redis >>/workspace/tmp/redis.log 2>>/workspace/tmp/redis.log &
 
 # Wait for mysql to start then setup the database if needed
--- .devcontainer/setup_formatif_db.sh
+++ .devcontainer/setup_formatif_db.sh
@@ -68,6 +68,7 @@ echo "Database created - you can open another terminal while this completes if y
 
 echo "Simulating sign off in the background - this may take a while.... but you can get started working - we are all setup"
 
+sudo rm -rf /workspace/tmp/database_populate.log
 bundle exec rake db:simulate_signoff >>/workspace/tmp/database_populate.log 2>>/workspace/tmp/database_populate.log
 rm /workspace/tmp/database_populate.log
 
