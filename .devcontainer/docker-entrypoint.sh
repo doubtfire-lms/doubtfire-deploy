1a2305bb80f7ea65f807c3db54aa0c253cac4781
Andrew Cain <macite@gmail.com>
2025-04-01 21:32:04 +1100

    chore: update dev compose to use new setup
    - remove redis/data startup
    - use build images

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -6,9 +6,9 @@ echo "Starting Formatif development container..."
 # sudo -E /workspace/.devcontainer/launch_db.sh mysqld
 
 # Start the redis server
-mkdir -p /workspace/tmp/sidekiq-redis
-sudo rm -rf /workspace/tmp/redis.log
-sudo redis-server --dir /workspace/tmp/sidekiq-redis >>/workspace/tmp/redis.log 2>>/workspace/tmp/redis.log &
+# mkdir -p /workspace/tmp/sidekiq-redis
+# sudo rm -rf /workspace/tmp/redis.log
+# sudo redis-server --dir /workspace/tmp/sidekiq-redis >>/workspace/tmp/redis.log 2>>/workspace/tmp/redis.log &
 
 # Wait for mysql to start then setup the database if needed
 /workspace/.devcontainer/setup_formatif_db.sh
